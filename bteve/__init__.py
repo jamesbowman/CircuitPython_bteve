2b632bf7fa77f2dc6820c57fb2e6ed9e8062e920
James Bowman <jamesb@excamera.com>
2021-07-19 18:50:43 -0700

    Movie player added to module exports

--- bteve/__init__.py
+++ bteve/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.1.2'
 from .gameduino_circuitpython import GameduinoCircuitPython
 Gameduino = GameduinoCircuitPython
 from .registers import *
-from .eve import align4
+from .eve import align4, MoviePlayer
